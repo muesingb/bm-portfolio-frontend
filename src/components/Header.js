460a21cbc676c3875b3098826e7e2c168fd5f196
Barbara <muesingb@gmail.com>
2020-03-22 13:13:44 -0400

    setup

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <>
+            <h1>hiiiii</h1>
+        </>
+    )
+}
+export default Header;
---
+++ src/containers/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../components/Header'
+
+const Main = () => {
+    return (
+        <>
+            <Header />
+        </>
+    )
+}
+export default Main;
