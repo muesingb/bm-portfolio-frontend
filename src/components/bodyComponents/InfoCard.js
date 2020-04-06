9ef15225d74dd62282f3206a779100626d4403ba
Barbara <muesingb@gmail.com>
2020-04-06 13:54:50 -0400

    rendering basic layout

--- src/components/bodyComponents/InfoCard.js
+++ src/components/bodyComponents/InfoCard.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const InfoCard = (props) => {
-    console.log(props)
+    const [infoCard, changeInfoCard] = useState([])
+
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_API_URI}/infocards/${props.id}`)
+              .then(response => response.json())
+              .then(data => {
+                changeInfoCard(data)
+                console.log(data.img_url)
+              }).catch(function(err) {
+                console.log(err);
+              })
+    }, [])
+
+    //{infoCard.img_url}
     return (
         <>
-            <h1>InfoCard</h1>
+            <h1>{infoCard.text}</h1>
+            {infoCard.img_url ? <img src="https://i.redd.it/jab5c3kfm8s21.jpg" /> : null}
         </>
     )
 }
